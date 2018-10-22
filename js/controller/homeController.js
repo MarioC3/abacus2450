42fd7a903aa5147c4070d92e4d1bd5c1d3e510cb
Mario A. Carlos <me@macarlos.com>
2018-10-22 01:56:07 -0600

    Better organization for CSS files, and built to select the levels.

---
+++ js/controller/homeController.js
@@ -0,0 +1,26 @@
+//Modal
+let getStartedButton = $(".getStartedButton");
+getStartedButton.click(()=> {
+    //open modal
+    let modal = $(".modal");
+    let modalContent = $('.modal-content');
+    modal.addClass("is-active animated fadeIn");
+    modalContent.addClass("animated bounceInUp");
+
+    //close modal
+    let closeModal = () => {
+        console.log('works')
+        modal.removeClass("is-active");   
+    }
+    let modalClose = $(".modal-close");
+    let modalBackground = $(".modal-background")
+    modalClose.click(()=> {
+        closeModal();
+    })
+    modalBackground.click(()=> {
+        closeModal();
+    })
+})
+
+
+
